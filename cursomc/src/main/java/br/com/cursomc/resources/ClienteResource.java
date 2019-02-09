c5fa6c152165431ebb8a381ee1c3d0f4ee285090
Rodrigo Costa Santos <rodrigocostasantos07@gmail.com>
2019-02-09 17:24:53 -0200

    resource

---
+++ cursomc/src/main/java/br/com/cursomc/resources/ClienteResource.java
@@ -0,0 +1,28 @@
+package br.com.cursomc.resources;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.cursomc.domain.Categoria;
+import br.com.cursomc.domain.Cliente;
+import br.com.cursomc.services.CategoriaService;
+import br.com.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService clienteService;
+
+	@RequestMapping(value="/{id}",method=RequestMethod.GET)
+	public ResponseEntity<?> listar(@PathVariable Integer id) {
+		Cliente cliente = clienteService.buscar(id);
+		return ResponseEntity.ok(cliente);
+	}
+}
---
+++ cursomc/src/main/java/br/com/cursomc/services/ClienteService.java
@@ -0,0 +1,20 @@
+package br.com.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.cursomc.domain.Cliente;
+import br.com.cursomc.repositories.ClienteRepository;
+import br.com.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	public Cliente buscar(Integer id) {
+		return clienteRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Cliente não encontrada @id= " + id));
+	}
+	
+}
