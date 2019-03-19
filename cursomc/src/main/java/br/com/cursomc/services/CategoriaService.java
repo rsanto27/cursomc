a1bd6a14859c57793e0ac05fd3475d1d7622f0e3
Rodrigo Costa <rodrigocosta@softbox.com.br>
2019-03-19 15:49:40 -0300

    delete

--- cursomc/src/main/java/br/com/cursomc/services/CategoriaService.java
+++ cursomc/src/main/java/br/com/cursomc/services/CategoriaService.java
@@ -1,10 +1,12 @@
 package br.com.cursomc.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.cursomc.domain.Categoria;
 import br.com.cursomc.repositories.CategoriaRepository;
+import br.com.cursomc.services.exceptions.DataIntegrityException;
 import br.com.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -25,5 +27,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return catRepository.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			catRepository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não foi possível excluir uma categoria que possui produtos");
+		}
 	}
 }
