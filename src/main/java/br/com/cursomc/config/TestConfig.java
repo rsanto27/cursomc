25c85ade7790153e3328f16f7da522a07d686f0e
Rodrigo Costa <rodrigocosta@softbox.com.br>
2019-05-13 12:51:15 -0300

    adicionando security

--- src/main/java/br/com/cursomc/config/TestConfig.java
+++ src/main/java/br/com/cursomc/config/TestConfig.java
@@ -11,26 +11,23 @@ import br.com.cursomc.services.DBService;
 import br.com.cursomc.services.EmailService;
 import br.com.cursomc.services.MockEmailService;
 
-//Configuração do profile de teste que fica dentro de resources
-//Todos os beans aqui instanciados serao efetivos apenas quando o profile de teste estiver ativo
+
+
 @Configuration
 @Profile("test")
 public class TestConfig {
 
 	@Autowired
 	private DBService dbService;
-
-	// Bean com os inserts e updates gerados para produzir a massa de dados do
-	// profile test
+	
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 		dbService.instantiateTestDatabase();
 		return true;
 	}
-
+	
 	@Bean
 	public EmailService emailService() {
 		return new MockEmailService();
 	}
-
 }
